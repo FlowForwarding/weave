ec66766b971a20c4ac1fa199910d7543d1c35664
Magnus Henoch <magnus.henoch@gmail.com>
2015-03-30 12:00:21 +0100

    Base flow rules on IP addresses
    Include IP addresses of endpoints in JSON file.
    
    Instead of indiscriminately forwarding everything from one port to
    another, use more discriminating flow rules:
    
    - Forward IP traffic with the desired src/dst address pair, in both
      directions.
    - Forward ARP traffic with the desired target protocol address, in both
      directions.

--- src/fc_find_path.erl
+++ src/fc_find_path.erl
@@ -12,7 +12,11 @@
 path_flow_rules(Ep1, Ep2) ->
     {ok, Graph} = dobby_oflib:get_path(Ep1, Ep2),
     VerticesEdges = vertices_edges_path(Graph, Ep1, Ep2),
-    convert_return_value(flow_rules(VerticesEdges, Graph)).
+    {Ep1, endpoint, #{<<"ip">> := #{value := Ip1Bin}}} = hd(VerticesEdges),
+    {ok, Ip1} = inet:parse_ipv4strict_address(binary_to_list(Ip1Bin)),
+    {Ep2, endpoint, #{<<"ip">> := #{value := Ip2Bin}}} = lists:last(VerticesEdges),
+    {ok, Ip2} = inet:parse_ipv4strict_address(binary_to_list(Ip2Bin)),
+    convert_return_value(flow_rules(VerticesEdges, Graph, Ip1, Ip2)).
 
 vertices_edges_path(Graph, Ep1, Ep2) ->
     Path = digraph:get_path(Graph, Ep1, Ep2),
@@ -31,36 +35,57 @@ vertices_edges([Id1, Id2 | _] = [Id1 | Tail], Graph) ->
     [{Id1, binary_to_atom(Type1, utf8), Metadata1}, binary_to_atom(EdgeType, utf8)]
 	++ vertices_edges(Tail, Graph).
 
--spec flow_rules([_], digraph:graph()) -> flow_path().
-flow_rules(Path, Graph) ->
-    flow_rules(Path, Graph, #{}).
+-spec flow_rules([_], digraph:graph(), inet:ip4_address(), inet:ip4_address()) -> flow_path().
+flow_rules(Path, Graph, Ip1, Ip2) ->
+    flow_rules(Path, Graph, #{}, Ip1, Ip2).
 
-flow_rules([{_, endpoint, _}], _Graph, FlowRulesMap) ->
+flow_rules([{_, endpoint, _}], _Graph, FlowRulesMap, _Ip1, _Ip2) ->
     %% Nothing more to do.
     FlowRulesMap;
-flow_rules([{_, endpoint, _}, connected_to | [{_, of_port, _} | _] = Tail], Graph, FlowRulesMap) ->
-    flow_rules(Tail, Graph, FlowRulesMap);
-flow_rules([{_, of_port, _}, connected_to | [{_, of_port, _} | _] = Tail], Graph, FlowRulesMap) ->
-    flow_rules(Tail, Graph, FlowRulesMap);
-flow_rules([{_, of_port, _}, connected_to | [{_, endpoint, _} | _] = Tail], Graph, FlowRulesMap) ->
-    flow_rules(Tail, Graph, FlowRulesMap);
+flow_rules([{_, endpoint, _}, connected_to | [{_, of_port, _} | _] = Tail], Graph, FlowRulesMap, Ip1, Ip2) ->
+    flow_rules(Tail, Graph, FlowRulesMap, Ip1, Ip2);
+flow_rules([{_, of_port, _}, connected_to | [{_, of_port, _} | _] = Tail], Graph, FlowRulesMap, Ip1, Ip2) ->
+    flow_rules(Tail, Graph, FlowRulesMap, Ip1, Ip2);
+flow_rules([{_, of_port, _}, connected_to | [{_, endpoint, _} | _] = Tail], Graph, FlowRulesMap, Ip1, Ip2) ->
+    flow_rules(Tail, Graph, FlowRulesMap, Ip1, Ip2);
 flow_rules([{Port1, of_port, _}, port_of, {_Switch, of_switch, SwitchMetadata},
-	    port_of | [{Port2, of_port, _} | _] = Tail], Graph, FlowRulesMap) ->
+	    port_of | [{Port2, of_port, _} | _] = Tail], Graph, FlowRulesMap, Ip1, Ip2) ->
     %% Add bidirectional flow rules.
     SwitchId = maps:get(value, maps:get(<<"datapath_id">>, SwitchMetadata)),
     {?OF_VERSION, ExistingRules} = maps:get(SwitchId, FlowRulesMap, {?OF_VERSION, []}),
-    Rule1 = {
-      [{in_port, fc_utils:id_to_port_no(Port1)}],
-      [{apply_actions, [{output, fc_utils:id_to_port_no(Port2), no_buffer}]}],
+    FromPort1 = {in_port, fc_utils:id_to_port_no(Port1)},
+    ToPort2 = {apply_actions, [{output, fc_utils:id_to_port_no(Port2), no_buffer}]},
+    IpTrafficThere = {
+      [FromPort1,
+       {ipv4_src, Ip1},
+       {ipv4_dst, Ip2}],
+      [ToPort2],
       [{table_id, 0},
        {cookie, unique_cookie()}]},
-    Rule2 = {
-      [{in_port, fc_utils:id_to_port_no(Port2)}],
-      [{apply_actions, [{output, fc_utils:id_to_port_no(Port1), no_buffer}]}],
+    ArpPacketsThere = {
+      [FromPort1,
+       {arp_tpa, ip_to_bin(Ip2)}],
+      [ToPort2],
       [{table_id, 0},
        {cookie, unique_cookie()}]},
-    NewFlowRulesMap = maps:put(SwitchId, {?OF_VERSION, [Rule1, Rule2] ++ ExistingRules}, FlowRulesMap),
-    flow_rules(Tail, Graph, NewFlowRulesMap).
+    FromPort2 = {in_port, fc_utils:id_to_port_no(Port2)},
+    ToPort1 = {apply_actions, [{output, fc_utils:id_to_port_no(Port1), no_buffer}]},
+    IpTrafficBack = {
+      [FromPort2,
+       {ipv4_src, Ip2},
+       {ipv4_dst, Ip1}],
+      [ToPort1],
+      [{table_id, 0},
+       {cookie, unique_cookie()}]},
+    ArpPacketsBack = {
+      [FromPort2,
+       {arp_tpa, ip_to_bin(Ip1)}],
+      [ToPort1],
+      [{table_id, 0},
+       {cookie, unique_cookie()}]},
+    NewRules = [IpTrafficThere, ArpPacketsThere, IpTrafficBack, ArpPacketsBack],
+    NewFlowRulesMap = maps:put(SwitchId, {?OF_VERSION, NewRules ++ ExistingRules}, FlowRulesMap),
+    flow_rules(Tail, Graph, NewFlowRulesMap, Ip1, Ip2).
 
 edges_between(Id1, Id2, Graph) ->
     OutEdges1 = digraph:out_edges(Graph, Id1),
@@ -77,3 +102,6 @@ unique_cookie() ->
     {A,B,C} = erlang:now(),
     N = (A * 1000000 + B) * 1000000 + C,
     <<N:64>>.
+
+ip_to_bin({A,B,C,D}) ->
+    <<A:8,B:8,C:8,D:8>>.
