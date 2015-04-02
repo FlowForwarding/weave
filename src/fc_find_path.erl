c711a59390f5f55b1890b61f98052bdc18818d54
Magnus Henoch <magnus.henoch@gmail.com>
2015-04-02 16:14:51 +0100

    Update for new dobby_oflib API
    Call dobby_oflib:publish_net_flow.
    Publish flow mods to Dobby separately, using dobby_oflib:publish_dp_flow_mod.

--- src/fc_find_path.erl
+++ src/fc_find_path.erl
@@ -18,7 +18,7 @@ path_flow_rules(Ep1, Ep2) ->
     {ok, Ip1} = inet:parse_ipv4strict_address(binary_to_list(Ip1Bin)),
     {Ep2, endpoint, #{<<"ip">> := #{value := Ip2Bin}}} = lists:last(VerticesEdges),
     {ok, Ip2} = inet:parse_ipv4strict_address(binary_to_list(Ip2Bin)),
-    convert_return_value(flow_rules(VerticesEdges, Graph, Ip1, Ip2)).
+    flow_rules(VerticesEdges, Graph, Ip1, Ip2).
 
 vertices_edges_path(Graph, Ep1, Ep2) ->
     Path = digraph:get_path(Graph, Ep1, Ep2),
@@ -39,22 +39,21 @@ vertices_edges([Id1, Id2 | _] = [Id1 | Tail], Graph) ->
 
 -spec flow_rules([_], digraph:graph(), inet:ip4_address(), inet:ip4_address()) -> [datapath_flow_mod()].
 flow_rules(Path, Graph, Ip1, Ip2) ->
-    flow_rules(Path, Graph, #{}, Ip1, Ip2).
+    flow_rules(Path, Graph, [], Ip1, Ip2).
 
-flow_rules([{_, endpoint, _}], _Graph, FlowRulesMap, _Ip1, _Ip2) ->
+flow_rules([{_, endpoint, _}], _Graph, FlowRules, _Ip1, _Ip2) ->
     %% Nothing more to do.
-    FlowRulesMap;
-flow_rules([{_, endpoint, _}, connected_to | [{_, of_port, _} | _] = Tail], Graph, FlowRulesMap, Ip1, Ip2) ->
-    flow_rules(Tail, Graph, FlowRulesMap, Ip1, Ip2);
-flow_rules([{_, of_port, _}, connected_to | [{_, of_port, _} | _] = Tail], Graph, FlowRulesMap, Ip1, Ip2) ->
-    flow_rules(Tail, Graph, FlowRulesMap, Ip1, Ip2);
-flow_rules([{_, of_port, _}, connected_to | [{_, endpoint, _} | _] = Tail], Graph, FlowRulesMap, Ip1, Ip2) ->
-    flow_rules(Tail, Graph, FlowRulesMap, Ip1, Ip2);
+    FlowRules;
+flow_rules([{_, endpoint, _}, connected_to | [{_, of_port, _} | _] = Tail], Graph, FlowRules, Ip1, Ip2) ->
+    flow_rules(Tail, Graph, FlowRules, Ip1, Ip2);
+flow_rules([{_, of_port, _}, connected_to | [{_, of_port, _} | _] = Tail], Graph, FlowRules, Ip1, Ip2) ->
+    flow_rules(Tail, Graph, FlowRules, Ip1, Ip2);
+flow_rules([{_, of_port, _}, connected_to | [{_, endpoint, _} | _] = Tail], Graph, FlowRules, Ip1, Ip2) ->
+    flow_rules(Tail, Graph, FlowRules, Ip1, Ip2);
 flow_rules([{Port1, of_port, _}, port_of, {_Switch, of_switch, SwitchMetadata},
-	    port_of | [{Port2, of_port, _} | _] = Tail], Graph, FlowRulesMap, Ip1, Ip2) ->
+	    port_of | [{Port2, of_port, _} | _] = Tail], Graph, FlowRules, Ip1, Ip2) ->
     %% Add bidirectional flow rules.
     SwitchId = maps:get(value, maps:get(<<"datapath_id">>, SwitchMetadata)),
-    {?OF_VERSION, ExistingRules} = maps:get(SwitchId, FlowRulesMap, {?OF_VERSION, []}),
     FromPort1 = {in_port, fc_utils:id_to_port_no(Port1)},
     ToPort2 = {apply_actions, [{output, fc_utils:id_to_port_no(Port2), no_buffer}]},
     IpTrafficThere = {
@@ -86,19 +85,13 @@ flow_rules([{Port1, of_port, _}, port_of, {_Switch, of_switch, SwitchMetadata},
       [{table_id, 0},
        {cookie, unique_cookie()}]},
     NewRules = [IpTrafficThere, ArpPacketsThere, IpTrafficBack, ArpPacketsBack],
-    NewFlowRulesMap = maps:put(SwitchId, {?OF_VERSION, NewRules ++ ExistingRules}, FlowRulesMap),
-    flow_rules(Tail, Graph, NewFlowRulesMap, Ip1, Ip2).
+    NewFlowRules = [{SwitchId, ?OF_VERSION, NewRule} || NewRule <- NewRules] ++ FlowRules,
+    flow_rules(Tail, Graph, NewFlowRules, Ip1, Ip2).
 
 edges_between(Id1, Id2, Graph) ->
     OutEdges1 = digraph:out_edges(Graph, Id1),
     InEdges2 = digraph:in_edges(Graph, Id2),
     [Edge || Edge <- OutEdges1, lists:member(Edge, InEdges2)].
-
-%% @doc Convert flow paths to what `dobby_oflib:publish_new_flow'
-%% expects.
-convert_return_value(FlowRules) ->
-    %% It expects a list of tuples of the form {Dpid, {OFVersion, FlowMods}}.
-    maps:to_list(FlowRules).
 
 unique_cookie() ->
     {A,B,C} = erlang:now(),
--- src/flowcompiler.erl
+++ src/flowcompiler.erl
@@ -15,15 +15,20 @@ setup_flow(Source, Destination)
   when is_binary(Source), is_binary(Destination) ->
     FlowRules = fc_find_path:path_flow_rules(Source, Destination),
     lists:foreach(fun send_flow_rules/1, FlowRules),
-    dobby_oflib:publish_new_flow(
+    FlowModIds =
+        lists:map(
+          fun(DatapathFlowMod) ->
+                  {ok, FlowModId} =
+                      dobby_oflib:publish_dp_flow_mod(<<"flowcompiler">>, DatapathFlowMod),
+                  FlowModId
+          end, FlowRules),
+    dobby_oflib:publish_net_flow(
       <<"flowcompiler">>,
       Source,
       Destination,
-      FlowRules).
+      FlowModIds).
 
-send_flow_rules({Dpid, {OFVersion, Flows}}) when is_binary(Dpid) ->
-    lists:map(
-      fun({Matches, Instr, Opts}) ->
-              Msg = of_msg_lib:flow_add(OFVersion, Matches, Instr, Opts),
-              {ok, noreply} = ofs_handler:sync_send(binary_to_list(Dpid), Msg)
-      end, Flows).
+send_flow_rules({Dpid, OFVersion, {Matches, Instr, Opts}}) when is_binary(Dpid) ->
+    Msg = of_msg_lib:flow_add(OFVersion, Matches, Instr, Opts),
+    {ok, noreply} = ofs_handler:sync_send(binary_to_list(Dpid), Msg),
+    ok.
