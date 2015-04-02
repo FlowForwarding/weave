adf6ddcfd5a4accd96753d74761d4a5c81579095
Magnus Henoch <magnus.henoch@gmail.com>
2015-04-02 16:18:09 +0100

    Add 'weave' command line tool

--- src/flowcompiler.erl
+++ src/flowcompiler.erl
@@ -1,6 +1,7 @@
 -module(flowcompiler).
 
--export([setup_flow/2]).
+-export([setup_flow/2,
+         main/0]).
 
 %% @doc Set up a net flow from `Source' to `Destination'.
 %%
@@ -32,3 +33,65 @@ send_flow_rules({Dpid, OFVersion, {Matches, Instr, Opts}}) when is_binary(Dpid) 
     Msg = of_msg_lib:flow_add(OFVersion, Matches, Instr, Opts),
     {ok, noreply} = ofs_handler:sync_send(binary_to_list(Dpid), Msg),
     ok.
+
+main() ->
+    [JSONFile, SourceS, DestinationS] = init:get_plain_arguments(),
+    {ok, _} = application:ensure_all_started(dobby),
+    {ok, _} = application:ensure_all_started(flowcompiler),
+    %% Wait for Dobby's Mnesia table.
+    ok = mnesia:wait_for_tables([identifier], 10000),
+
+    %% Import the JSON file.  Assume that it's in the legacy format
+    %% for now.
+    case dby_bulk:import(json0, JSONFile) of
+        ok -> ok;
+        {error, ImportError} ->
+            io:format(standard_error, "Cannot import JSON file ~s: ~p",
+                      [JSONFile, ImportError]),
+            halt(1)
+    end,
+
+    Source = list_to_binary(SourceS),
+    Destination = list_to_binary(DestinationS),
+    %% Ready to find flow rules!
+    FlowRules = fc_find_path:path_flow_rules(Source, Destination),
+
+    %% Now that we know which switches are affected by the flow rules,
+    %% ensure that they are connected.
+    case lists:foldl(fun wait_for_switch/2, 10, lists:ukeysort(1, FlowRules)) of
+        0 ->
+            io:format(standard_error, "Timeout while waiting for switches\n", []),
+            halt(1);
+        Remaining when is_integer(Remaining) ->
+            ok
+    end,
+
+    lists:foreach(fun send_flow_rules/1, FlowRules),
+    FlowModIds =
+        lists:map(
+          fun(DatapathFlowMod) ->
+                  {ok, FlowModId} =
+                      dobby_oflib:publish_dp_flow_mod(<<"flowcompiler">>, DatapathFlowMod),
+                  FlowModId
+          end, FlowRules),
+    dobby_oflib:publish_net_flow(
+      <<"flowcompiler">>,
+      Source,
+      Destination,
+      FlowModIds),
+    io:format("~b rules published\n", [length(FlowRules)]),
+    halt(0).
+
+wait_for_switch({Dpid, _, _} = FlowRule, Remaining) ->
+    case lists:keymember(binary_to_list(Dpid), 2, AllSwitches = simple_ne_logic:switches()) of
+        true ->
+            io:format("Switch ~s is online\n", [Dpid]),
+            Remaining;
+        false when Remaining =< 0 ->
+            io:format("Timeout waiting for switch ~s\n", [Dpid]),
+            Remaining;
+        false ->
+            io:format("Switch ~s is offline; waiting... (~p)\n", [Dpid, AllSwitches]),
+            timer:sleep(1000),
+            wait_for_switch(FlowRule, Remaining - 1)
+    end.
